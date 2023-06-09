904ddf8faac590e5a8c1c3f5fb2700335683ad49
borkab <borbala.botond@gmail.com>
2023-06-09 08:58:14 +0200

    .

--- 07/maps.go
+++ 07/maps.go
@@ -4,12 +4,17 @@ type Dictionary map[string]string
 
 const ErrNotFound = DictionaryErr("couldn't find the word you were looking for")
 const ErrWordExists = DictionaryErr("this word is already exist")
+const ErrWordDoesNotExist = DictionaryErr("this word still does not exist")
 
-type DictionaryErr string
+type DictionaryErr string //mivel implementalja az error interfacet, csinalhatunk belole konstanst lsd.fent
 
-func (e DictionaryErr) Error() string {
+// Any type with the Error()string method fulfils the error interface
+// It looks similar to the errors.errorString() implementation that powers erorrs.New()
+// Howerver unlike errors.errorString() this type is a constant expression
+
+func (e DictionaryErr) Error() string { //emiatt a DictionaryErr tipusunk implementalja az error interfacet
 	return string(e)
-}
+} //https://dave.cheney.net/2016/04/07/constant-errors
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -33,4 +38,24 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 
 	return nil
+}
+
+// Update() takes 2 strings as arguments, which are a key and a new value for this key
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word) //a search() altal megtalalt szo valtozojara itt nincs szuksegem, csak a hibauzenetre
+
+	switch err { //megnezzuk milyen hibak tortenhetnek
+	case ErrNotFound: //ha nem talaltuk meg a szot
+		return ErrWordDoesNotExist //adja vissza, h ez a szo meg nem letezik a szotarunkban
+	case nil: //ha hiba nelkul lezajlott a kereses, hajtsa vegre a frissitest
+		d[word] = newDefinition //megadom a d nevu map-nak hogy a word kulcs mezonek a newDefinition stringet adja meg erteknek
+	default: //alapesetben pedig (azaz minden mas esetben)
+		return err //adja vissza az err valtozoban eltarolt hibauzenetet
+	}
+
+	return nil //a funkcion pedig adjon vissza egy nil erteku hibauzit ha lezajlott a frissites
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
--- 07/maps_test.go
+++ 07/maps_test.go
@@ -52,6 +52,46 @@ func TestAdd(t *testing.T) {
 	})
 }
 
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "pulp"
+		definition := "fiction"
+		dictionary := Dictionary{word: definition} //feltoltom adattal a Dictionary{} mappet
+		// hogy tudjunk benne frissiteni
+		newDefinition := "fucking.fiction" //az uj definicio
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)                             //megnezem, h az update() method visszaadott error erteke nil-e(azaz hiba nelkul futott e le a frissites)
+		assertDefinition(t, dictionary, word, newDefinition) //megnezem h update-eles utan benne van e dictionary mapben a word key az uj definicioval
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "pulp"
+		definition := "fiction"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist) //ellenorzom, h az update() visszaadott error valtozo erteke megegyezik-e a konstans hibauzenet ertekevel,
+		//tehat a frissites tenylegesen azert nem ment vegbe, mert nincs benne ez a szo a dictionary map-ben
+	})
+}
+
+func TestDelete(t *testing.T) {
+	word := "cinderella"
+	dictionary := Dictionary{word: "tests a shoe"}
+
+	dictionary.Delete(word)
+
+	_, err := dictionary.Search(word)
+	if err != ErrNotFound {
+		t.Errorf("Expected %q to be deleted", word)
+	}
+
+}
+
+// assertDefinition() ellenorzom, h a dictionary nevu mapben benne van a word key es a hozzatartozo definition value
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
@@ -64,12 +104,15 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 	assertStrings(t, got, definition)
 }
 
+// assertError() ellenorzom h ket error tipusu ertek egyforma e
 func assertError(t *testing.T, got, want error) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+// asserrtStrings() ellenorzom, h ket string tipusu ertek egyforma e
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
